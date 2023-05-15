ad89f850983fd77140213a8444d59b4cbec7df7b
Rianrohimat <47967313+Rianrohimat@users.noreply.github.com>
2023-05-15 17:43:36 +0700

    minigolf

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+	public GameObject hole1Par;
+	public GameObject hole2Par;
+	public GameObject hole3Par;
+	public GameObject totalPar;
+	
+	public GameObject hole1Strokes;
+	public GameObject hole2Strokes;
+	public GameObject hole3Strokes;
+	public GameObject totalStrokes;
+    // Start is called before the first frame update
+    void Start()
+    {
+		switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    hole1Par.GetComponent<TMPro.TextMeshProUGUI>().text = "4";
+					hole2Par.GetComponent<TMPro.TextMeshProUGUI>().text = "4";
+					hole3Par.GetComponent<TMPro.TextMeshProUGUI>().text = "4";
+					totalPar.GetComponent<TMPro.TextMeshProUGUI>().text = "12";
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    hole1Par.GetComponent<TMPro.TextMeshProUGUI>().text = "3";
+					hole2Par.GetComponent<TMPro.TextMeshProUGUI>().text = "3";
+					hole3Par.GetComponent<TMPro.TextMeshProUGUI>().text = "3";
+					totalPar.GetComponent<TMPro.TextMeshProUGUI>().text = "9";
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    hole1Par.GetComponent<TMPro.TextMeshProUGUI>().text = "2";
+					hole2Par.GetComponent<TMPro.TextMeshProUGUI>().text = "2";
+					hole3Par.GetComponent<TMPro.TextMeshProUGUI>().text = "2";
+					totalPar.GetComponent<TMPro.TextMeshProUGUI>().text = "6";
+                    break;
+            }
+			
+		hole1Strokes.GetComponent<TMPro.TextMeshProUGUI>().text = GameManager.hole1.ToString();
+		hole2Strokes.GetComponent<TMPro.TextMeshProUGUI>().text = GameManager.hole2.ToString();
+		hole3Strokes.GetComponent<TMPro.TextMeshProUGUI>().text = GameManager.hole3.ToString();
+		totalStrokes.GetComponent<TMPro.TextMeshProUGUI>().text = (GameManager.hole1 + GameManager.hole2 + GameManager.hole3 ).ToString();
+
+        switch (DifficultyManager.Difficulty)
+        {
+            case DifficultyManager.Difficulties.easy:
+                GameManager.maxStrokes = 20;
+                GameManager.strokes = 20;
+                break;
+            case DifficultyManager.Difficulties.normal:
+                GameManager.maxStrokes = 15;
+                GameManager.strokes = 15;
+                break;
+            case DifficultyManager.Difficulties.hard:
+                GameManager.maxStrokes = 10;
+                GameManager.strokes = 10;
+                break;
+        }
+    }
+
+    public void PlayGame()
+    {
+        GameManager.hole1 = 0;
+        GameManager.hole2 = 0;
+        GameManager.hole3 = 0;
+       
+        SceneManager.LoadScene("hole1");
+    }
+	
+	public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour { 
+
+    public static int hole1 = 0;
+    public static int hole2 = 0;
+    public static int hole3 = 0;
+    public static int hole4 = 0;
+    public static int hole5 = 0;
+    public static int hole6 = 0;
+    public static int hole7 = 0;
+    public static int hole8 = 0;
+    public static int hole9 = 0;
+    public static int maxStrokes = 15;
+    public static int strokes = maxStrokes;
+    public Text curStrokes;
+    public static int currentHole = 0;
+    public static int par = 0;
+    public Text curPar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (DifficultyManager.Difficulty)
+        {
+            case DifficultyManager.Difficulties.easy:
+                maxStrokes = 20;
+                strokes = 20;
+                break;
+            case DifficultyManager.Difficulties.normal:
+                maxStrokes = 15;
+                strokes = 15;
+                break;
+            case DifficultyManager.Difficulties.hard:
+                maxStrokes = 10;
+                strokes = 10;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "hole1")
+        {
+            currentHole = hole1;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 3;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 2;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole2")
+        {
+            currentHole = hole2;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 3;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 2;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole3")
+        {
+            currentHole = hole3;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 3;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 2;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole4")
+        {
+            currentHole = hole4;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 5;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 3;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole5")
+        {
+            currentHole = hole5; ;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 5;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 3;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole6")
+        {
+            currentHole = hole6;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 3;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 2;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole7")
+        {
+            currentHole = hole7;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 5;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 3;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole8")
+        {
+            currentHole = hole8;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 3;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 2;
+                    break;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "hole9")
+        {
+            currentHole = hole9;
+            switch (DifficultyManager.Difficulty)
+            {
+                case DifficultyManager.Difficulties.easy:
+                    par = 5;
+                    break;
+                case DifficultyManager.Difficulties.normal:
+                    par = 4;
+                    break;
+                case DifficultyManager.Difficulties.hard:
+                    par = 3;
+                    break;
+            }
+        }
+
+        curStrokes.GetComponent<Text>().text = "Strokes: " + currentHole.ToString() + "/" + maxStrokes.ToString();
+        curPar.GetComponent<Text>().text = "Par: " + par.ToString();
+    }
+}
